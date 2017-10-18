4bf583b580e5ec7ad7ae029c67716356f75b5e87
Paul Gowder <paul.gowder@gmail.com>
2017-10-18 14:22:36 -0500

    async hytp with transducer works

--- src/scrapecontracts/scraper/requests.clj
+++ src/scrapecontracts/scraper/requests.clj
@@ -1,8 +1,14 @@
 (ns scrapecontracts.scraper.requests
   (:require [org.httpkit.client :as http]
-            [clojure.core.async :refer [go go-loop chan >! <!]]))
+            [clojure.core.async :refer [go go-loop chan >! <!]]
+            [scrapecontracts.scraper.soup :refer [parse]]))
 
-(def inchan (chan))
+(def parse-transducer
+  (comp
+   (map :body)
+   (map parse)))
+
+(def inchan (chan 1 parse-transducer))
 
 (defn- get [inchan address]
   (http/get address #(go (>! inchan %))))
@@ -11,7 +17,7 @@
   (go-loop []
     (swap! outatom conj (<! inchan))
     (recur)))
-;; this is just a stub for looking at results, will be replaced with actual processor code.  Listen to channel and dump everything in it into an atom.
+;; this is just a stub for looking at results, will be replaced with actual processor code.  Listen to channel and dump everything in it into an atom.  Or maybe I can just put all the side-effecting stuff in the transducer too and not have a go-loop or an atom at all?
 
 ;; atom is meant to be an empty vector
 
