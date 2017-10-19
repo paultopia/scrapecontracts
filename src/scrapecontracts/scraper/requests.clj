1f2b44bec5c10ef70c7cac60623dab8c987a9399
Paul Gowder <paultopia@users.noreply.github.com>
2017-10-18 22:15:47 -0500

    experimental reshape of fetch/getter

--- src/scrapecontracts/scraper/requests.clj
+++ src/scrapecontracts/scraper/requests.clj
@@ -13,27 +13,33 @@
 ;; error handling needs to be somewhere either in get or in parse-transduer
 
 
-(defn make-http-channel [addresses]
+(defn- make-http-channel [addresses]
   (let [inchan (chan 1 parse-transducer)]
     (dorun
      (map (partial get inchan) addresses))
     inchan))
 
-(defn watch-http-channel [http-channel, callback]
+(defn- watch-http-channel [http-channel, callback]
   (go-loop []
     (callback (<! http-channel))
     (recur)))
 
 
 (defn fetch [addresses callback]
-  (let [inchan (make-http-channel addresses)]
-    (watch-http-channel inchan callback)
-    (partial get inchan)))
+  (let [inchan (make-http-channel addresses)
+        getter (partial get inchan)
+        cb (partial callback getter)]
+    (watch-http-channel inchan cb)
+    getter))
 
 ;; example:
 ;; (def outatom (atom []))
-;; (fetch ["http://paul-gowder.com" "http://rulelaw.net"] #(swap! outatom conj %))
+;; (fetch ["http://paul-gowder.com" "http://rulelaw.net"] #(swap! outatom conj %2))
 ;; (count @outatom) => 2
 
 
-;; after fetch is called the first time, I can use the function returned to handle recursive crawling by just calling it to push new results onto the channel.
+;; callback function has special requirements: will take getter as its first argument and result of transducer on http-channel as second argument.  When not a crawler, the first argument won't be used and can be discarded (thus the example that just uses the second argument).  When it is a crawler, the callback can use the getter to queue up links found in the results for recursive scraping.  
+
+;; For example, I think that (defn callback [getter item] (dorun (map getter (:links item)))) should work to just take every link from every page and fetch that too, and so forth, recursively...
+
+;; this new thing noted in previous 2 comments is TOTALLY UNTESTED.  
