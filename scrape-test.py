1e22706c125a5c251f71bec93cb89864f41603fd
Paul Gowder <paultopia@users.noreply.github.com>
2017-11-14 16:20:57 -0600

    cleanup/refactor scraping code

--- scrape-test.py
+++ scrape-test.py
@@ -1,18 +1,28 @@
 
 # coding: utf-8
 
-# In[1]:
+# currently untested refactor
 
 
-import scrapy, pickle, json
+import requests, pickle, json, sklearn
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
-import sklearn
+from dirtyclean import clean
+from urllib.parse import urljoin
+from time import sleep
 
+browheader = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7"}
 
-# In[4]:
-
+def fetch(link):
+    time.sleep(1)
+    try:
+        response = requests.get(link, timeout=2, headers=browheader)
+    except:
+        return None
+    if response.status_code == 200:
+        return BeautifulSoup(response.text, "lxml")
+    return None
 
 with open("trained_contract_model.pickle", "rb") as pm:
     model = pickle.load(pm)
@@ -22,44 +32,28 @@ with open("sites_to_scrape.json") as sj:
     sites = json.load(sj)
 
 
-# In[11]:
-
-
-from dirtyclean import clean
 def isContract(newdata):
+    if newdata is None:
+        return False
     newdata = [newdata]
     transformed = transformer.transform([clean(x) for x in newdata])
     predictions = model.predict_proba(transformed)
     result = predictions[:,1] > 0.5
     return result[0]
 
-
-# In[12]:
-
-
-isContract("this is a cat, kitty kitty cat meow, also here is some totally random text that hopefully isn't full of stopwords")
-
-
-# In[51]:
-
-
-# you know what?  Maybe I won't bother with scrapy here.  It isn't an amazing match for this kind of project.
-import requests
-from urllib.parse import urljoin
-
-browheader = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7"}
-
 def normalize_link(baseurl, href):
     if href.startswith('http'):
         return href.partition("#")[0]
     return urljoin(baseurl, href).partition("#")[0]
 
-def extract_links(page, baseurl):
-    soup = BeautifulSoup(page, "lxml")
+def extract_links(soup, baseurl):
+    if soup is None:
+        return None
     return list(set([normalize_link(baseurl, x.get("href")) for x in soup.find_all('a')]))
 
-def extract_text(page):
-    soup = BeautifulSoup(page, "lxml")
+def extract_text(soup):
+    if soup is None:
+        return None
     for crap in soup(["script", "style", "meta"]):
         crap.extract()
     text = soup.get_text()
@@ -69,15 +63,11 @@ def extract_text(page):
     return text
     
 def check_for_TOS(link):
-    try:
-        response = requests.get(link, timeout=2, headers=browheader)
-    except:
-        return None
-    if response.status_code == 200:
-        text = extract_text(response.text)
-        if isContract(text):
-            return text
-        return None
+    soup = fetch(link)
+    text = extract_text(soup)
+    if isContract(text):
+        return text
+    return None
     
 def findTOS(site):
     site1 = "https://" + site
@@ -86,62 +76,26 @@ def findTOS(site):
     site4 = "http://www." + site
     try:
         target = site1
-        response = requests.get(target, timeout=2, headers=browheader)
+        soup = fetch(target)
     except:
         try:
             target = site2
-            response = requests.get(target, timeout=2, headers=browheader)
+            soup = fetch(target)
         except:
             try:
                 target = site3
-                response = requests.get(target, timeout=2, headers=browheader)
+                soup = fetch(target)
             except:
                 try:
                     target = site4
-                    response = requests.get(target, timeout=2, headers=browheader)
+                    soup = fetch(target)
                 except:
-                    response = None
-    if response:
-        if response.status_code == 200:
-            links = extract_links(response.text, target)
-            return [check_for_TOS(link) for link in links]
+                    soup = None
+    links = extract_links(soup, target)
+        if links:
+            return [x for x in [check_for_TOS(link) for link in links] if x is not None]
+        return None
 
-#findTOS("rulelaw.net")
+# so this exposes a simple API that I can just call from other code: loop over the sites to check and call findTOS on each.  Then for each return from that, I can check if it's none, and if not, append the contents to database. 
 
-
-# In[52]:
-
-
-# ok it's a misidentification but that's ok, there will be scraping errors.  let's see how this works on a site with a tos in a link.
-
-experiment = findTOS("www.moneycrashers.com")
-
-
-# In[53]:
-
-
-print(experiment)
-
-
-# In[54]:
-
-
-# ok, that's a bit of an issue --- I'm probably overidentifying because of the disclaimer text at bottom there.  let's try another
-experiment2 = findTOS("chroniclevitae.com")
-print(experiment2)
-
-
-# In[55]:
-
-
-# probably rate-limited me.  ugh.
-experiment3 = findTOS("zyl.ai")
-print(experiment3)
-
-
-# In[ ]:
-
-
-# sweet, that works.  I should put in a bit of a delay between requests via a time.sleep, but then I can shove it into production.
-# I'm happy with some false positives, they can be filtered out by humans.
-
+# or I can just stick the database code in here and have done with it. 
