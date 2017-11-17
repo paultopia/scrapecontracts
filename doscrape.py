d0a8cdb3086709ef07b2c93ed06eadfe57ee0663
Paul Gowder <paultopia@users.noreply.github.com>
2017-11-17 11:59:36 +0100

    fix db connection typo

--- doscrape.py
+++ doscrape.py
@@ -3,27 +3,26 @@ import sys
 import sqlite3
 import logging
 import pandas as pd
-from kscrape import findTOS
-from kdatabase import create_database, add_data
+from scrape import findTOS
+from database import create_database, add_data
 
-logging.basicConfig(filename='scraping.log',level="INFO")
+logging.basicConfig(filename='scraping.log',level=logging.info)
 
 def scrape_sites(jsonfile, dbconn):
     for site in jsonfile:
         contracts = findTOS(site)
-        if contracts:
-            for contract in contracts:
-                add_data(dbconn, contract["url"], contract["text"])
-                logging.info("attempted to add: " + contract["url"]) # if these don't show up in db, I know it failed and I can go back and redo more aggressively.
+        for contract in contracts:
+            add_data(dbconn, contract["url"], contract["text"])
+            logging.info("attempted to add: " + contract["url"]) # if these don't show up in db, I know it failed and I can go back and redo more aggressively.
     dbconn.close()
 
 def save_sites_as_csv(dbfilename, csvfilename): # separate fx because may blow up memory.
-    dbconnconn = sqlite3.connect(dbfilename)
+    dbconn = sqlite3.connect(dbfilename)
     try:
         df = pd.read_sql_query("SELECT * from contracts", dbconn)
         df.write_csv("csvfilename")
     except Exception as e:
-        logging.error("failed to load or write csv. here's the error: " + str(e))
+        logging.error("failed to load or write csv. here's the error: " + str(e.value))
     dbconn.close()
 
 
@@ -33,7 +32,7 @@ if __name__ == "__main__":
     jsonfile = sys.argv[1]
     dbfilename = sys.argv[2]
     csvfilename = sys.argv[3]
-    dbconn = create_database(dbfilename)
+    dbconn = create_datbase(dbfilename)
     scrape_sites(jsonfile, dbconn)
     save_sites_as_csv(dbfilename, csvfilename)
     print("done executing scraping program!")
